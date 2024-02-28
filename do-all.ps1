eb358d5813808e5429c58a911aa0fae0c4f15793
Kaleb Pederson <kpederson@healthequity.com>
2024-02-28 13:58:32 -0700

    Use terraform executable name instead of custom alias.

--- do-all.ps1
+++ do-all.ps1
@@ -1,14 +1,26 @@
+# make sure that terraform is available in the path
+if (-not (test-path terraform))
+{
+  Write-Error "Terraform is not installed or not in the path."
+  exit 1
+}
+
 # get the project name from the first argument if .local.tfvars does not exist
 if (-not (test-path .local.tfvars))
 {
   Write-Error "Create a .local.tfvars file specifying the project name prior to executing."
-  exit 1
+  exit 2
 }
 
 if (-not (test-path .terraform))
 {
   Write-Host "Initializing Terraform"
-  tf init
+  terraform init
+  if ($? -ne $true)
+  {
+    Write-Error "ERROR: 'terraform init' failed."
+    exit 3
+  }
 }
 else
 {
@@ -16,19 +28,19 @@ else
 }
 
 Write-Host "Creating Terraform plan"
-tf plan -var-file=".local.tfvars" -out=".tfplan"
+terraform plan -var-file=".local.tfvars" -out=".tfplan"
 if ($? -ne $true)
 {
-  Write-Error "ERROR: 'tf plan -var-file=`".local.tfvars`" -out=`".tfplan`"' failed."
-  exit 2
+  Write-Error "ERROR: 'terraform plan -var-file=`".local.tfvars`" -out=`".tfplan`"' failed."
+  exit 4
 }
 
 Write-Host "Applying Terraform plan"
-tf apply -auto-approve .tfplan
+terraform apply -auto-approve .tfplan
 if ($? -ne $true)
 {
-  Write-Error "ERROR: 'tf apply -auto-approve -out=.tfplan' failed."
-  exit 3
+  Write-Error "ERROR: 'terraform apply -auto-approve -out=.tfplan' failed."
+  exit 5
 }
 
 Write-Host "Getting project name from .local.tfvars"
@@ -36,7 +48,7 @@ $slsResult = sls -Pattern 'project_name\s*=\s*"([^"]+)"' .local.tfvars -Context 
 if ($slsResult.Matches.Count -eq 0)
 {
   Write-Error "Unable to find project_name in .local.tfvars"
-  exit 4
+  exit 6
 }
 else
 {
@@ -46,4 +58,8 @@ else
 
 Write-Host "Getting aks credentials for project '$projectName'"
 az aks get-credentials --name "$($projectName)-aks" --resource-group "$($projectName)-rg"
-
+if ($? -ne $true)
+{
+  Write-Error "ERROR: 'az aks get-credentials --name `"$($projectName)-aks`" --resource-group `"$($projectName)-rg`"' failed."
+  exit 7
+}
